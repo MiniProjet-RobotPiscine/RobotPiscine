aa26df2cb94cdc21b777cde302eb3fb5cef28948
Eric Bernauer <eric.bernauer@limayrac.fr>
2020-11-22 23:34:10 +0100

    Ajout d'un fichier .gitignore pour MPLAB

--- RobotPiscine.X/build/default/production/_ext/1821769327/commandesMoway.o.d
+++
@@ -1,6 +0,0 @@
-build/default/production/_ext/1821769327/commandesMoway.o:  \
-	Z:/Annee2019-2020/1SN/RobotPiscine/ProjetRobotPiscine/ProjetRobotPiscine/CodeProjet/sources/commandesMoway.c  \
-	Z:/Annee2019-2020/1SN/RobotPiscine/ProjetRobotPiscine/ProjetRobotPiscine/CodeProjet/includes/lib_sen_moway.h  \
-	Z:/Annee2019-2020/1SN/RobotPiscine/ProjetRobotPiscine/ProjetRobotPiscine/CodeProjet/includes/lib_mot_moway.h  \
-	Z:/Annee2019-2020/1SN/RobotPiscine/ProjetRobotPiscine/ProjetRobotPiscine/CodeProjet/includes/commandesMoway.h  \
-
--- RobotPiscine.X/build/default/production/_ext/1821769327/main.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/_ext/1821769327/main.o:  \
-	Z:/Annee2019-2020/1SN/RobotPiscine/ProjetRobotPiscine/ProjetRobotPiscine/CodeProjet/sources/main.c  \
-	Z:/Annee2019-2020/1SN/RobotPiscine/ProjetRobotPiscine/ProjetRobotPiscine/CodeProjet/includes/robot.h  \
-
